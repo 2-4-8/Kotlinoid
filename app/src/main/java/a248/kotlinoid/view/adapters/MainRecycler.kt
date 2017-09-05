b6fc8caa7d8122f987378d5b2f3cf51be6f52a04
lkayda <ankayda@gmail.com>
2017-09-05 19:32:48 +0500

    data binding added to main activity and recycler adapter

--- app/src/main/java/a248/kotlinoid/view/adapters/MainRecycler.kt
+++ app/src/main/java/a248/kotlinoid/view/adapters/MainRecycler.kt
@@ -1,6 +1,7 @@
 package a248.kotlinoid.view.adapters
 
 import a248.kotlinoid.R
+import a248.kotlinoid.databinding.RecyclerItemBinding
 import a248.kotlinoid.model.ItemEntity
 import android.graphics.Rect
 import android.support.v7.widget.RecyclerView
@@ -10,23 +11,16 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.recycler_item.view.*
 
-fun ViewGroup.inflate(layoutRes: Int): View {
-    return LayoutInflater.from(context).inflate(layoutRes, this, false)
-}
-
 class MainRecyclerAdapter(
         var items: List<ItemEntity>,
         val onClick: (ItemEntity) -> Unit,
         val deleter: (ItemEntity) -> Unit) : RecyclerView.Adapter<MainRecyclerAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        fun bind(item: ItemEntity,
-                 onClick: (ItemEntity) -> Unit,
-                 deleter: (ItemEntity) -> Unit) = with(itemView) {
-            item_title.text = "${item.title} (#${item.uuid})"
-            item_desc.text = item.desc
-            item_delete.setOnClickListener { deleter(item) }
-            itemView.setOnClickListener { onClick(item) }
+    class ViewHolder(val binding: RecyclerItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ItemEntity, onClick: (ItemEntity) -> Unit, deleter: (ItemEntity) -> Unit) {
+            binding.item = item
+            binding.itemDelete.setOnClickListener{ deleter(item) }
+            binding.root.setOnClickListener{ onClick(item) }
         }
     }
 
@@ -34,7 +28,7 @@ class MainRecyclerAdapter(
             = holder.bind(items[position], onClick, deleter)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
-            = ViewHolder(parent.inflate(R.layout.recycler_item))
+            = ViewHolder(RecyclerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
     override fun getItemCount(): Int = items.size
 }
